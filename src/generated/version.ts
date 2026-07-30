5e8e0d6d80bd93d9e35c67c15f3d7f3a094e6455
YellMoon <yellmoon@users.noreply.github.com>
2026-07-30 23:01:28 +0800

    release: unify version 7.2.0

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated - do not edit
-// Updated: 2026-07-30T11:40:51.706Z
-// Build: 20260730-1940
-export const APP_VERSION = "7.1.1";
-export const BUILD_TAG = "20260730-1940";
+// Updated: 2026-07-30T15:00:59.155Z
+// Build: 20260730-2300
+export const APP_VERSION = "7.2.0";
+export const BUILD_TAG = "20260730-2300";
