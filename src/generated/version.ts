582453094b8e255f3c5686f4d4b888c8490cbb16
YellMoon <yellmoon@users.noreply.github.com>
2026-09-26 17:58:07 +0800

    自动发布 2026-09-26：桌面 8.9.18

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated - do not edit
-// Updated: 2026-09-25T23:20:49.406Z
-// Build: 20260926-0720
-export const APP_VERSION = "8.9.17";
-export const BUILD_TAG = "20260926-0720";
+// Updated: 2026-09-26T09:52:30.380Z
+// Build: 20260926-1752
+export const APP_VERSION = "8.9.18";
+export const BUILD_TAG = "20260926-1752";
